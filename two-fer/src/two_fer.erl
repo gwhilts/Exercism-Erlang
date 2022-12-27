2959f6159bc306a8705e4ba3ab3297bd9c7b85ad
Gerald Hilts <gwhilts@booknowl.com>
2022-12-26 18:13:37 -0800

    Implement 'Two-fer' exercise (accepted)

--- two-fer/src/two_fer.erl
+++ two-fer/src/two_fer.erl
@@ -1,8 +1,11 @@
 -module(two_fer).
+-import(string,[concat/2]). 
 
 -export([two_fer/0, two_fer/1]).
 
 
-two_fer() -> undefined.
+two_fer() -> 
+  two_fer("you").
 
-two_fer(_Name) -> undefined.
+two_fer(Name) -> 
+  ["One for ", Name, ", one for me."].
