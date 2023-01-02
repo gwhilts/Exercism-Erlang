091fcbeb0b6fa7a16a6cfa8a1fe81deabfa291f5
Gerald Hilts <gwhilts@booknowl.com>
2023-01-01 23:25:04 -0800

    Implement 'Leap' exercise (submitted)

--- leap/src/leap.erl
+++ leap/src/leap.erl
@@ -2,5 +2,10 @@
 
 -export([leap_year/1]).
 
-
-leap_year(_Year) -> undefined.
+leap_year(Year) -> 
+  if
+    Year rem 400 == 0 -> true;
+    Year rem 100 == 0 -> false;
+    Year rem 4 == 0 -> true;
+    true -> false
+  end.
